594275fd2425397d9c6de4b1ba2756b3aff91631
CristhianRiveraR <riverarodriguezcristhian@hotmail.com>
2021-11-11 23:37:55 -0600

    Se establece conexion con la API

--- app/src/main/java/com/example/randog/MainActivity.java
+++ app/src/main/java/com/example/randog/MainActivity.java
@@ -3,12 +3,155 @@ package com.example.randog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.X509TrustManager;
+
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+import java.util.ResourceBundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnConsultar;
+    TextView txtPrb;
+    RequestQueue requestQueue;
+
+    static final String URL = "https://random.dog/woof.json";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        requestQueue = Volley.newRequestQueue(this);
+        initCompnents();
+
+        fixSsl();
+
+        btnConsultar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stringRequest();
+            }
+        });
+
+    }
+
+    private void initCompnents(){
+        btnConsultar = findViewById(R.id.btnConsultar);
+        txtPrb = findViewById(R.id.txtPrb);
+    }
+
+    private void stringRequest(){
+
+        StringRequest request = new StringRequest(
+                Request.Method.GET,
+                URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        //txtPrb.setText(response);
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String pesoImg = jsonObject.getString("fileSizeBytes");
+                            String urlImg = jsonObject.getString("url");
+
+                            txtPrb.setText(urlImg);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        int x= 0;
+                    }
+                }
+        );
+        requestQueue.add(request);
+    }
+
+    private void fixSsl(){
+        TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+            }
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+            }
+        } };
+        SSLContext sc = null;
+        try {
+            sc = SSLContext.getInstance("SSL");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        try {
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        }
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        // Create all-trusting host name verifier
+        HostnameVerifier allHostsValid = new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        };
+        // Install the all-trusting host verifier
+        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+    }
+    private void getPerro(){
+/*
+        StringRequest postRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    String pesoImg = jsonObject.getString("fileSizeBytes");
+                    String urlImg = jsonObject.getString("url");
+
+                    txtPrb.setText(urlImg);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                txtPrb.setText(error.getMessage());
+            }
+        });
+
+        Volley.newRequestQueue(this).add(postRequest);
+        */
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/randog/models/Perro.java
@@ -0,0 +1,14 @@
+package com.example.randog.models;
+
+public class Perro {
+    private String fileSizeBytes,
+                    url;
+
+    public String getFileSizeBytes() {
+        return fileSizeBytes;
+    }
+
+    public void setFileSizeBytes(String fileSizeBytes) {
+        this.fileSizeBytes = fileSizeBytes;
+    }
+}
